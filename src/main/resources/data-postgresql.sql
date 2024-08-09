6cf3388933ab9948a0fa1ba6c9356be90d5bc4b5
devhong <popodaddy@gmail.com>
2024-08-09 20:43:12 +0900

    pgcrypto 생성 중복 제거

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -3,5 +3,5 @@ INSERT INTO posts(title, content, author, created_at) VALUES ('title-2', 'conten
 INSERT INTO posts(title, content, author, created_at) VALUES ('title-3', 'content', 'nomad.coding', now());
 INSERT INTO posts(title, content, author, created_at) VALUES ('title-4', 'content', 'nomad.coding', now());
 
-CREATE EXTENSION IF NOT EXISTS pgcrypto; //pgcrypto 확장 설치 활성화
+--CREATE EXTENSION IF NOT EXISTS pgcrypto; //pgcrypto 확장 설치 활성화
 INSERT INTO users(email, password, name) VALUES ('admin@gmail.com', crypt('1234', gen_salt('bf')), 'nomad.coding');
